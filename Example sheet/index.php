8275c10170f5633922e3aed472cd5c7388c87277
Jenelle Williams <williams1189@mail.philau.edu>
2017-09-14 20:39:21 -0400

    starting over

---
+++ Example sheet/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><?php echo 'hello world';?></p>
+    <?php
+      $arrayName = array('key' => , );
+
+      $i - 0;
+      $array_count = count($array);
+    for ($i=0; $i < $array_count; $i++) {
+      echo "<br/>keys in loop";
+    }
+    // function get_stuff($args,$things){
+    //
+    // }
+    // $thing = get_stuff($args);
+     ?>
+  </body>
+</html>
