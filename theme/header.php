840900418f7d707d31ec7381493f9a5c289de517
Jenelle Williams <williams1189@mail.philau.edu>
2017-10-05 19:45:38 -0400

    changes

--- theme/header.php
+++ theme/header.php
@@ -1,11 +1,26 @@
-<!doctype html>
-<html>
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title><?= isset($PageTitle) ? $PageTitle : "Default Title"?></title>
-    <!-- Additional tags here -->
-    <?php if (function_exists('customPageHeader')){
-      customPageHeader();
-    }?>
-  </head>
-  <body>
+<!DOCTYPE html>
+    <html>
+      <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <title><?= isset($PageTitle) ? $PageTitle : "Default Title"?></title>
+
+        ;/
+        <?php if (function_exists('customPageHeader')){
+          customPageHeader();
+        }?>
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body <?php body_class(); ?>>
+      <?php
+                $PageTitle="Home";
+      ?>
+          <ul>
+          <li<?php if($_GET['page']=="home")   { echo " class=\"active\""; } ?>><a href="?page=home"> Home</a></li>
+          <li<?php if($_GET['page']=="about")   { echo " class=\"active\""; } ?>><a href="?page=about"> About</a></li>
+          <li<?php if($_GET['page']=="portfolio")  { echo " class=\"active\""; } ?>><a href="?page=portfolio"> Portfolio</a></li>
+          <li<?php if($_GET['page']=="contact"){ echo " class=\"active\""; } ?>><a href="?page=contact"> Contact</a></li>
+          </ul>
+
+          <div class="banner">
+              <img id="banner" src="img/me.jpg" alt=""/>
+          </div>
--- theme/index.php
+++ theme/index.php
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-    <?php
+<?php get_header(); ?>
+<main>
 
-      $PageTitle="Home";
-      function customPageHeader(){?>
-      <?php }
-      include_once('header.php');
+  <h1><?php the_title(); ?></h1>
 
-      <ul class="nav">
-        <li><a href="home.php">Home</a></li>
-        <li><a href="portfolio.php">Portfolio</a></li>
-        <li class="right"><a href="about.php">About</a></li>
-        <li class="right"><a href="contact.php">Contact</a></li>
-      </ul>
-      <div class="banner">
-          <img id="banner" src="img/me.jpg" alt=""/>
-      </div>
+</main>
+<?php get_footer(); ?>
 
-      include_once('footer.php');
-      ?>
-  </body>
-</html>
+<body <?php body_class("home"); ?>>
